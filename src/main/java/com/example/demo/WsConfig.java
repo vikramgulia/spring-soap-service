277fa70925225f8014e5c5de1919d070851ee737
vikram gulia <vgulia@hawk.iit.edu>
2018-11-26 19:49:52 -0700

    Update WsConfig.java

--- src/main/java/com/example/demo/WsConfig.java
+++ src/main/java/com/example/demo/WsConfig.java
@@ -1,29 +1,22 @@
 package com.example.demo;
 
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.ApplicationContext;
+import javax.xml.ws.Endpoint;
+
+import org.apache.cxf.Bus;
+import org.apache.cxf.jaxws.EndpointImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.ws.config.annotation.EnableWs;
-import org.springframework.ws.transport.http.MessageDispatcherServlet;
-import org.springframework.ws.wsdl.WsdlDefinition;
-import org.springframework.ws.wsdl.wsdl11.SimpleWsdl11Definition;
 
-@EnableWs
 @Configuration
 public class WsConfig {
-    @Bean
-    public ServletRegistrationBean<MessageDispatcherServlet> messageDispatcherServlet(ApplicationContext applicationContext) {
-        MessageDispatcherServlet servlet = new MessageDispatcherServlet();
-        servlet.setApplicationContext(applicationContext);
-        servlet.setTransformWsdlLocations(true);
-        return new ServletRegistrationBean<MessageDispatcherServlet>(servlet, "/ws/*");
-    }
+    @Autowired
+    private Bus bus;
 
-    @Bean("GetWeather")
-    public WsdlDefinition getWeatherWsdl() {
-        WsdlDefinition wsdlDefinition = new SimpleWsdl11Definition(new ClassPathResource("wsdl/GetWeather.wsdl"));
-        return wsdlDefinition;
+    @Bean
+    public Endpoint endpoint() {
+        EndpointImpl endpoint = new EndpointImpl(bus, new GetWeather());
+        endpoint.publish("/");
+        return endpoint;
     }
 }
