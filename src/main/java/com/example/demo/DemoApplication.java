a5f4d4aa5d5c5437a46895b8b7052a47fc4ef30b
vikram gulia <vgulia@hawk.iit.edu>
2018-11-26 19:50:12 -0700

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,15 +1,10 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
-
-    @Autowired
-    ConfigurableApplicationContext cac;
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
